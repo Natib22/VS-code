e1b2e64fd8fb89f59a4dd797efe6628f4c3c083f
Natnael <natibir400@gmail.com>
2024-01-26 21:45:41 +0300

    dont know

--- exam.cpp
+++ exam.cpp
@@ -1,17 +1,37 @@
-#include<iostream>
+#include <iostream>
+#include <fstream>
+#include <string>
 using namespace std;
-int reference(int &num);
 
-int main(){
-    int x=100;
-    reference(x);
-     cout<<" main function manipulated "<<x;
-    return 0;
-}
-int reference(int &num){
+int main()
+{
+    // string my[6];
+    // ifstream myfile;
+    // string reader;
+    // myfile.open("dum.txt", ios::in);
 
-    cout<<"passed "<<num<<endl;
-    num=10;
-    cout<<"changed "<<num<<endl; 
-   return num;
+    // if (myfile.is_open())
+    // {
+    //     getline(myfile, reader);
+    //     cout << reader;
+    //     for (int i = 0, j = 0; i < 5 && j < reader.length(); i++, j++)
+    //     {
+    //         // Use single quotes to compare with the character ','
+    //         while (reader[j] != ',' && j < reader.length())
+    //         {
+    //             my[i] += reader[j];
+    //             j++;
+    //         }
+
+    //         cout << my[i] << endl;
+    //     }
+
+    //     myfile.close();
+    // }
+    // else
+    // {
+    //     cout << "Unable to open file" << endl;
+    // }
+
+    // return 0;
 }
